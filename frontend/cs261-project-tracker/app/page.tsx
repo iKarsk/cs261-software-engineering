4c466146ba3e19c43672dfd884f40ac384c0dd77
iKarsk <krystian.karski@outlook.com>
2023-02-16 18:27:27 +0000

    Prisma and route

--- frontend/cs261-project-tracker/app/page.tsx
+++ frontend/cs261-project-tracker/app/page.tsx
@@ -1,12 +1,12 @@
 import styles from './page.module.css'
-
+import Link from 'next/link';
 
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <p><span className={styles.magic}>Hello.</span></p>
-      <p className={styles.subtext}>Homepage.</p>
+      <p className={styles.subtext}></p>
     </div>
   )
 }
