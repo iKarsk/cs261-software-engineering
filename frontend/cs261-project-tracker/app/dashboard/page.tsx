7a5d78f57a8ced21649b55f5d0b533f13d1e615b
iKarsk <krystian.karski@outlook.com>
2023-03-02 14:05:17 +0000

    Populate projects, create new project.

--- frontend/cs261-project-tracker/app/dashboard/page.tsx
+++ frontend/cs261-project-tracker/app/dashboard/page.tsx
@@ -16,23 +16,64 @@ import {
     ModalCloseButton,
   } from '@chakra-ui/react'
 
+  import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    InputGroup,
+    InputLeftElement
+  } from '@chakra-ui/react'
+
 export default function Dashboard() {
     const {status, data} = useSession();
     const { isOpen: isInviteOpen, onOpen: onInviteOpen, onClose: onInviteClose } = useDisclosure();
     const { isOpen: isProjectOpen, onOpen: onProjectOpen, onClose: onProjectClose } = useDisclosure();
-    const [loading, setLoading] = useState(false);
     
+    const [loading, setLoading] = useState(false);
+    const [invites, setInvites] = useState([]);
+    
+    const [projects, setProjects] = useState([]);
+    const [projectsLoading, setProjectsLoading] = useState(false);
+
+    const [newProject, setNewProject] = useState({});
+
     useEffect(() => {
         if (status === "unauthenticated") redirect("/login");
-    }, [status]);
 
+        if (status === "authenticated") {
+            setProjectsLoading(true);
+            const endpoint = "/api/user/getAllProjects";
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({userid: data?.user.id}),
+            };
+
+            fetch(endpoint, options)
+            .then((response) => response.json())
+            .then((responseJSON) => {
+                setProjects(responseJSON);
+                setProjectsLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+
+    }, [status]);
 
     
     const handleInvites = async () => {
         setLoading(true);
         onInviteOpen();
+
         const postData = {
-            id: data?.user.id,
+            userid: data?.user.id,
         };
 
         const JSONdata = JSON.stringify(postData);
@@ -49,14 +90,34 @@ export default function Dashboard() {
         };
         const response = await fetch(endpoint, options);
 
-        console.log(response);
+        const responseJSON = await response.json();
 
+        setInvites(responseJSON);
 
-        
+        setLoading(false);        
 
     }
 
     if (status === "authenticated"){
+
+        const displayInvites = () => {
+
+            if(invites.length){
+                const inviteList = invites.map((invite) => {
+                    return (
+                        <div>
+                            <h3>Invite Name</h3>
+                        </div>
+                    )
+                });
+    
+                return inviteList;    
+            }else{
+                return <h3>You have no invites</h3>
+            }
+
+        }
+
         return (
             <div className={styles.container}>
                 <h1>Welcome! You are signed in and can access this page.</h1>
@@ -68,7 +129,9 @@ export default function Dashboard() {
 
                 <Button onClick={onProjectOpen} mt={5}>Create new Project</Button>
 
-                <Box bg='tomato' w='20%' h='100px' mt={5} color='white' display='flex' justifyContent='center' alignItems='center'>sdf</Box>
+                <Box bg='tomato' w='20%' h='100px' mt={5} color='white' display='flex' justifyContent='center' alignItems='center'>
+                    {projectsLoading ? "Loading..." : projects.length ? "You have projects" : "You have no projects"}
+                </Box>
 
                 <Modal
                     isOpen={isInviteOpen}
@@ -80,7 +143,8 @@ export default function Dashboard() {
                         <ModalHeader>Project Invites</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody pb={6}>
-                            Hello!
+                            {loading ? "Loading..." : displayInvites()}
+                            
                         </ModalBody>
                         <ModalFooter>
                             <Button colorScheme='blue' mr={3}>
@@ -101,7 +165,38 @@ export default function Dashboard() {
                         <ModalHeader>Create a new project</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody pb={6}>
-                            Hello!
+                            <FormControl mt={4}>
+                                <FormLabel>Project Name</FormLabel>
+                                <Input placeholder="Project Name"/>
+                            </FormControl>
+
+                            <FormControl mt={4}>
+                                <FormLabel>Deadline</FormLabel>
+                                <Input placeholder="Select date" type="date"/>
+                            </FormControl>
+
+                            <FormControl mt={4}>
+                                <FormLabel>Budget</FormLabel>
+
+                                <InputGroup>
+                                    <InputLeftElement
+                                    pointerEvents='none'
+                                    color='gray.300'
+                                    fontSize='1.2em'
+                                    children='£'
+                                    />
+                                    <Input type="number" placeholder='Enter amount' />
+
+                                </InputGroup>
+                                
+                            </FormControl>
+
+                            <FormControl mt={4}>
+                                <FormLabel>Repository Link</FormLabel>
+                                <Input placeholder="https://"/>
+                            </FormControl>
+
+
                         </ModalBody>
                         <ModalFooter>
                             <Button colorScheme='blue' mr={3}>
