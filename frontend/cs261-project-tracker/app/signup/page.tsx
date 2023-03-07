4d5e83f0fb1a49cda5c7b87997816d18fd1222a0
iKarsk <krystian.karski@outlook.com>
2023-03-07 00:01:22 +0000

    Signup form styled

--- frontend/cs261-project-tracker/app/signup/page.tsx
+++ frontend/cs261-project-tracker/app/signup/page.tsx
@@ -4,14 +4,25 @@ import styles from './page.module.css'
 import Link from 'next/link';
 import { signIn, useSession } from "next-auth/react"
 import { redirect } from 'next/navigation';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Loading from '@/components/loading';
+import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, CircularProgress, InputGroup, InputRightElement } from '@chakra-ui/react';
+import ErrorMessage from '@/components/ErrorMessage';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
+
 
 export default function Home() {
     const {status, data} = useSession();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const passwordVisibility = () => setShowPassword(!showPassword);
 
     const handleSubmit = async (event : any) => {
         event.preventDefault();
+
+        setLoading(true);
 
         const data = {
             forename: event.target.forename.value,
@@ -39,12 +50,19 @@ export default function Home() {
         console.log(response);
 
         if (response.status === 200 || response.status === 201) {
+            setLoading(false);
             const res = await signIn('credentials', {
                 email: event.target.email.value,
                 password: event.target.password.value,
                 redirect: false,
                 callbackUrl: "/dashboard"
             });
+        } else if(response.status == 409){
+            setLoading(false);
+            setError("Email already exists");
+        }else{
+            setLoading(false);
+            setError("Internal server error. Please try again later.");
         }
     }
 
@@ -57,6 +75,51 @@ export default function Home() {
 
     if (status === "unauthenticated"){
         return (
+            
+            <Flex width="100vw" height="100vh" align="center" justifyContent="center">
+            <Box p={8} maxWidth="600px" borderWidth={1} borderRadius={8} boxShadow="lg">
+                <Box textAlign="center">
+                    <Heading>Sign Up</Heading>
+                </Box>
+                <Box my={4} textAlign="left">
+                    <form onSubmit = {handleSubmit}>
+                        {error && <ErrorMessage message={error} />}
+                        <FormControl isRequired>
+                            <FormLabel>Forename</FormLabel>
+                            <Input type='text' id='forename' placeholder='John' />
+                        </FormControl>
+                        <FormControl mt={6} isRequired>
+                            <FormLabel>Surname</FormLabel>
+                            <Input type='text' id='surname' placeholder='Doe' />
+                        </FormControl>
+                        <FormControl mt={6} isRequired>
+                            <FormLabel>Years of Experience</FormLabel>
+                            <Input type='number' id='experience' placeholder='0' />
+                        </FormControl>
+                        <FormControl mt={6} isRequired>
+                            <FormLabel>Email</FormLabel>
+                            <Input type="email" placeholder="johndoe@email.com" id="email"/>
+                        </FormControl>
+                        <FormControl mt={6} isRequired>
+                            <FormLabel>Password</FormLabel>
+                            <InputGroup>
+                                <Input type={showPassword ? 'text' : 'password'} placeholder="********" id="password" />
+                                <InputRightElement width="3rem">
+                                    <Button h="1.5rem" size="sm" onClick={passwordVisibility}>
+                                        {showPassword ? <ViewOffIcon /> : <ViewIcon />}
+                                    </Button>
+                                </InputRightElement>
+                            </InputGroup>
+                        </FormControl>
+                        <Button type="submit" colorScheme="teal" variant="outline" width="full" mt={4}>
+                            {loading ? (<CircularProgress isIndeterminate size="24px" color="teal" />) : ('Sign in')}
+                        </Button>
+                    </form>
+                </Box>
+            </Box>
+        </Flex>
+        
+            /*
             <div className={styles.container}>  
                 <form onSubmit = {handleSubmit}>
                     <div id={styles.formBox}>
@@ -80,6 +143,7 @@ export default function Home() {
                 </form>
                 
             </div>
+            */
         )
     }
 
