2203227c7562699b23baf7e5072a568d34a805fa
iKarsk <krystian.karski@outlook.com>
2023-03-09 17:19:48 +0000

    Button and text positioning

--- frontend/cs261-project-tracker/app/page.tsx
+++ frontend/cs261-project-tracker/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import styles from './page.module.css'
-import { Heading, Flex, Button, Kbd, Text, Box } from '@chakra-ui/react';
+import { Heading, Flex, Button, Kbd, Text, Spacer } from '@chakra-ui/react';
 import { ArrowForwardIcon } from '@chakra-ui/icons'
 import { useRouter } from "next/navigation";
 
@@ -10,16 +10,20 @@ export default function Home() {
 
   return (
     <Flex direction="column" align="center" justify="center" h="100vh">
+      <Spacer />
+      <Flex direction="column" align="center" justify="center">
       <Heading as="h1" size="3xl" mb={2}>Project Tracker</Heading>
       <Heading as="h2" size="md">A project management tool for CS261</Heading>
       <Button colorScheme="teal" mt={3} onClick={() => router.push("/login")}>Login</Button>
+      </Flex>
+      <Spacer />
 
-
+      <Flex direction="column" align="center" justify="center" mb={50} position="fixed" bottom="0">
       <Text mt={20} as="b" fontSize="lg" mb={1}>This website is accessible!</Text>
       <span>
-        Navigate with: &nbsp; <Kbd>tab</Kbd>, <Kbd>arrowkeys</Kbd> and <Kbd>enter</Kbd>
+        Navigate with: &nbsp; <Kbd>tab</Kbd>, <Kbd>arrowkeys</Kbd> and <Kbd>enter</Kbd>.
       </span>
-
+      </Flex>
     </Flex>
 
     /*
